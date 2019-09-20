e1e73d0940795c4d545ff8625840da188a7177a8
tahaamaidi92 <benhama.taha.33@gmail.com>
2019-09-20 02:58:12 +0100

    Updates

---
+++ precache-manifest.a0fd8e15c8b799e51846e774713c01c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92527df2446a5ddba334955e7788c3f4",
+    "url": "/CourseLists/index.html"
+  },
+  {
+    "revision": "f3dbefa3d965f60767d8",
+    "url": "/CourseLists/static/css/main.537132b8.chunk.css"
+  },
+  {
+    "revision": "1ea4c843b9129169b1c9",
+    "url": "/CourseLists/static/js/2.1c96ba98.chunk.js"
+  },
+  {
+    "revision": "f3dbefa3d965f60767d8",
+    "url": "/CourseLists/static/js/main.e17e1429.chunk.js"
+  },
+  {
+    "revision": "a8343ef400879b73eb4a",
+    "url": "/CourseLists/static/js/runtime~main.6c932ba2.js"
+  }
+]);
